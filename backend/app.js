e5c1254d6d0b81a057d4d521893de0e1879f0c3c
Mekin Jemal <mekinjemal999@gmail.com>
2025-01-02 09:15:42 +0300

    complete register and login functinality

--- backend/app.js
+++ backend/app.js
@@ -1,68 +1,69 @@
-import express from 'express';
-import mysql from 'mysql2';
-import cors from "cors";
-const port = 4000;
-const app = express();
+import express from "express"
+import mysql from "mysql2"
 
-app.use(express.json());
-app.use(cors({origin:"http://localhost:5173"}))
-// app.use(express.urlencoded({extended:true}));   
-const dbConfig = {
-   host:"localhost",
-   user:"root",
-   database:"demo",
-   password:"mubarek2012##"
+
+
+const app = express()
+app.use(express.json())
+
+const config= {
+  user:'root',
+  host:'localhost',
+  password:'1234',
+  database:"demo",
+  waitForConnections:true,
+  connectionLimit:10,
+  queueLimit:0,
+
 }
-const db = mysql.createConnection(dbConfig);
-db.connect((err)=>{
-   if(err){
-      console.log(err);
-   }
-   console.log("Database connected");
-});
 
-app.get("/",(req,res)=>{
-   res.send("hello world");
-});
+export const db=mysql.createPool(config)
+  // console.log(db)
 
-app.get("/employee",(req,res)=>{
-   const sql = "select * from employee_test";
-   db.query(sql,(err,result)=>{
-      if(err){
-         console.log(err);
-         return res.status(500).json({message:"error while try to fetch data from dtatabase",status:"failed"});
-      }
-      res.send(result);
-   });
+if(db){
+  console.log("Database connected successfully")
+}
+
+app.post("/add-user",(req,res)=>{
+  const {first_name,last_name,email,password}= req.body
+  // console.log(data)
+  const q='insert into auth  (first_name,last_name,email,password) values (?,?,?,?)'
+  db.query(q,[first_name,last_name,email,password], (err, results) => {
+    if(err) {
+      res.status(500).send("Error happening while inserting to the database")
+    } else {
+      res.status(200).json({
+        message:"User created successfully",
+        user:results
+      })
+    }
+  })
 })
-app.post("/employee",(req,res)=>{
-   console.log(req.body);
-
-   const {body:{first_name,last_name,email,password}} = req;
-   const value = [first_name, last_name, email, password];
-   const sql = "insert into employee_test(first_name, last_name, email, password) values(?,?,?,?)";
-   db.query(sql,value,(err)=>{
-      if(err){
-         console.log(err);
-         return res.status(500).json({message:"error while try to insert data into dtatabase",status:"failed"});
-      }
-      res.status(200).json({message:"success"});
-   });
-   // res.status(200).json({message:"success",data:body});
-});
 
 app.post("/login",(req,res)=>{
-   console.log(req.body)
-   const {body:{email,password}} = req;
-   const sql = "select * from employee_test where email = ? and password = ?"
-    db.query(sql,[email, password],(err,result)=>{
-      if(err){
-         console.log('error while try to fetch data from database',err);
-         return res.status(500).json({message:"failed",error:"can't fetch data from database"});
+  const {email,password}=req.body
+  const q=`select * from auth where email=?`
+  db.query(q,[email,password],(err,results)=>{
+     if(err){
+       res.status(500).send("Error happening while fetching data from the database")}
+      else{
+        if(results.length>0){
+          if(results[0].password===password){
+            res.status(200).json({
+              message:"User logged in successfully",
+              user:results[0]
+            })
+          }else{
+            res.status(400).send("Invalid password")
+          }
+        }else{
+          res.status(400).send("User not found")
+        }
       }
-      res.status(200).json({message:"success",data:result});
-   })
-});
-app.listen(port,()=>{
-   console.log(`Server is running on port ${port}`)
-});
+
+})
+})
+
+app.listen(3000,()=>{
+  console.log("Server is Running on port 3000")
+})
